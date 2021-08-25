a6f089f974b3adb10ccd9fd2b20ee76f6beba8b3
fikihfirmansyah <fikihfirmansyah43@gmail.com>
2021-08-25 10:43:59 +0700

    add

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'SUARA USU',
+    site_url: 'https://dev.suarausu.or.id',
+    feed_url: 'https://dev.suarausu.or.id/feed'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
